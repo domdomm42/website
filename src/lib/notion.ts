bc0e8f1c85420770d06e930006679790b793883a
Oudom <limoudom2001@gmail.com>
2024-12-27 18:20:02 +1100

    modify getPostsData to only query blog posts that are 'done'

--- src/lib/notion.ts
+++ src/lib/notion.ts
@@ -25,6 +25,12 @@ export async function getPostsData() {
         direction: "descending",
       },
     ],
+    filter: {
+      property: "Status",
+      select: {
+        equals: "Done",
+      },
+    },
   });
 
   return response.results
